a8903c532be177e00e9fb78572b02951e7924299
정이삭 <isaac56@naver.com>
2021-01-06 15:15:39 +0900

    Solve 백준 1000번

---
+++ src/backjun/backjun1000.java
@@ -0,0 +1,13 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class backjun1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
+        sc.close();
+    }
+}
