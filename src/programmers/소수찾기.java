b7cfee4c522e041d0aed7ac0e7d9ccb31d03f60d
정이삭 <isaac56@naver.com>
2021-01-13 23:19:16 +0900

    프로그래머스 소수찾기에 다른사람 풀이 추가

--- src/programmers/소수찾기.java
+++ src/programmers/소수찾기.java
@@ -1,17 +1,6 @@
 package programmers;
 import java.util.*;
 public class 소수찾기 {
-    public boolean isPrime(long num){
-        if(num == 0 || num == 1)
-            return false;
-
-        for(long i = 2; i <= (long)Math.sqrt((double)num); i++){
-            if(num%i == 0)
-                return false;
-        }
-        return true;
-    }
-
     public void permutation(StringBuilder builder, int depth, HashSet<Long> candiSet) {
         if(depth == builder.length() - 1) {
             for(int i = 0; i < builder.length(); i++) {
@@ -31,6 +20,26 @@ public class 소수찾기 {
         char temp = builder.charAt(i);
         builder.setCharAt(i, builder.charAt(j));
         builder.setCharAt(j, temp);
+    }
+
+    //다른사람의 풀이인데 심플하고 좋아 보여서 추가해놓았다.
+    public void permutation(String prefix, String str, HashSet<Long> candiSet){
+        if(!prefix.equals(""))
+            candiSet.add(Long.valueOf(prefix));
+        for(int i = 0; i < str.length(); i++) {
+            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, str.length()), candiSet);
+        }
+    }
+
+    public boolean isPrime(long num){
+        if(num == 0 || num == 1)
+            return false;
+
+        for(long i = 2; i <= (long)Math.sqrt((double)num); i++){
+            if(num%i == 0)
+                return false;
+        }
+        return true;
     }
 
     public int solution(String numbers) {
