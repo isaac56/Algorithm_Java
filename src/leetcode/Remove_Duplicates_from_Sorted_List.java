799d1d7e6490d3935ee7e0c229d60be1a8b90faa
정이삭 <isaac56@naver.com>
2021-01-13 16:35:29 +0900

    Remove Duplicates from Sorted List 문제 답안 주석 없이 컴파일 되도록 수정

--- src/leetcode/Remove_Duplicates_from_Sorted_List.java
+++ src/leetcode/Remove_Duplicates_from_Sorted_List.java
@@ -1,17 +1,25 @@
 package leetcode;
 
 public class Remove_Duplicates_from_Sorted_List {
-    //로컬에서는 컴파일이 안돼 주석처리함
-//    public ListNode deleteDuplicates(ListNode head) {
-//        ListNode curNode = head;
-//        while(curNode != null) {
-//            ListNode toNext = curNode.next;
-//            while(toNext != null && toNext.val == curNode.val) {
-//                toNext = toNext.next;
-//            }
-//            curNode.next = toNext;
-//            curNode = curNode.next;
-//        }
-//        return head;
-//    }
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curNode = head;
+        while(curNode != null) {
+            ListNode toNext = curNode.next;
+            while(toNext != null && toNext.val == curNode.val) {
+                toNext = toNext.next;
+            }
+            curNode.next = toNext;
+            curNode = curNode.next;
+        }
+        return head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
