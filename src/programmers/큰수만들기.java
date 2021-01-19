f70d1d38f4ff40356ccb1a8b26796bea630f63a1
정이삭 <isaac56@naver.com>
2021-01-20 00:49:40 +0900

    프로그래머스 큰수만들기 답지보고 Solve

--- src/programmers/큰수만들기.java
+++ src/programmers/큰수만들기.java
@@ -1,18 +1,23 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class 큰수만들기 {
     public String solution(String number, int k) {
         char[] charArr = number.toCharArray();
-        for(int i = 1; i <= k; i++) {
-            int stdIdx = charArr.length-1 - i;
-            for(int j = stdIdx+1; i < charArr.length; i++) {
-                if(charArr[stdIdx] < charArr[j]){
-                    char temp = charArr[stdIdx];
-                    charArr[stdIdx] = charArr[j];
+        for(int i = k - 1; i >=0; i--){
+            for(int j = k; j < charArr.length; j++) {
+                if(charArr[i] >= charArr[j]){
+                    char temp = charArr[i];
+                    charArr[i] = charArr[j];
                     charArr[j] = temp;
                 }
+                else
+                    break;
             }
         }
-        return String.valueOf(charArr).substring(0, number.length()-k + 1);
+
+        return String.valueOf(Arrays.copyOfRange(charArr, k, charArr.length));
     }
 }
+
