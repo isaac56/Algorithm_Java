ac5d4a0cc8bf5577bc1ddc5b13c34bc70e2f7e4d
정이삭 <isaac56@naver.com>
2021-03-10 22:14:13 +0900

    Modify 프로그래머스 타겟넘버
    가독성을 위해 변수명 변경

--- src/programmers/타겟넘버.java
+++ src/programmers/타겟넘버.java
@@ -1,15 +1,15 @@
 package programmers;
 
 public class 타겟넘버 {
-    public void dfs(int[] answer, int[] numbers, int target, int sum, int k){
-        if(k == numbers.length){
-            if(sum == target)
+    public void dfs(int[] answer, int[] numbers, int target, int sum, int depth) {
+        if (depth == numbers.length) {
+            if (sum == target)
                 answer[0]++;
             return;
         }
 
-        dfs(answer, numbers, target, sum + numbers[k], k+1);
-        dfs(answer, numbers, target, sum - numbers[k], k+1);
+        dfs(answer, numbers, target, sum + numbers[depth], depth + 1);
+        dfs(answer, numbers, target, sum - numbers[depth], depth + 1);
     }
 
     public int solution(int[] numbers, int target) {
