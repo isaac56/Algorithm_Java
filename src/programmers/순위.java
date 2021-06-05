a65286dff2372e1f01324b558f020e86248627e6
정이삭 <isaac56@naver.com>
2021-06-05 22:45:31 +0900

    Solve 프로그래머스 순위

---
+++ src/programmers/순위.java
@@ -0,0 +1,55 @@
+package programmers;
+
+import java.util.*;
+
+public class 순위 {
+    class Node {
+        int name;
+        Set<Node> upSet = new HashSet<>();
+        Set<Node> downSet = new HashSet<>();
+
+        Node(int name) {
+            this.name = name;
+        }
+
+        int getTotal() {
+            return upSet.size() + downSet.size();
+        }
+
+        void addUp(Node node) {
+            this.upSet.addAll(node.upSet);
+            this.upSet.add(node);
+            node.downSet.addAll(this.downSet);
+            node.downSet.add(this);
+
+            for (Node temp : node.downSet) {
+                temp.upSet.addAll(node.upSet);
+                temp.upSet.add(node);
+            }
+            for (Node temp : node.upSet) {
+                temp.downSet.addAll(this.downSet);
+                temp.downSet.add(this);
+            }
+        }
+    }
+
+    public int solution(int n, int[][] results) {
+        Map<Integer, Node> hm = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            hm.put(i, new Node(i));
+        }
+        for (int[] result : results) {
+            Node upNode = hm.get(result[0]);
+            Node downNode = hm.get(result[1]);
+            downNode.addUp(upNode);
+        }
+        int answer = 0;
+        for (Node node : hm.values()) {
+            if (node.getTotal() == n - 1) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
