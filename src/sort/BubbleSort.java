e8fe0b56eb1ac96ae4d1272a05ad1cf20dcd52af
정이삭 <isaac56@naver.com>
2021-06-18 20:14:05 +0900

    Implements Bubble Sort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,15 @@
+package sort;
+
+public class BubbleSort {
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/sort/SortTest.java
@@ -0,0 +1,23 @@
+package sort;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class SortTest {
+    private int[] array;
+    private int[] answer = new int[]{0, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 7, 8, 8, 8, 11, 32};
+
+    @BeforeEach
+    void setUp() {
+        array = new int[]{1, 5, 3, 6, 2, 4, 2, 6, 3, 5, 8, 3, 2, 4, 7, 8, 4, 4, 2, 3, 5, 8, 0, 11, 2, 32};
+    }
+
+    @Test
+    void BubbleSort() {
+        new BubbleSort().sort(array);
+        Assertions.assertTrue(Arrays.equals(array, answer));
+    }
+}
